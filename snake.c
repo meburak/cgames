8d9e26ea0cfa509e024aec4e2b8811eb5927a1af
meburak <105165961+meburak@users.noreply.github.com>
2026-02-08 21:48:51 +0300

    Update snake.c

--- snake.c
+++ snake.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
 
 int main(){
     int frame = 0;
     int headX = 0;
     int headY = 0;
-    char direction = 'D'; //D RIGHT MAX 10 MIN 0 COLUMNS J +
-                          //A LEFT MAX 0 MIN 10 COLUMNS J -
+    char direction = 'D'; //D LEFT MAX 10 MIN 0 COLUMNS J +
+                          //A RIGHT MAX 0 MIN 10 COLUMNS J -
                           //W UP MAX 0 MIN 10 ROWS I +
                           //S DOWN MIN 0 MAX 10 ROWS I -
+
+    struct termios oldattr, newattr; //OLD TERMINAL SETTINGS (GAME OFF) NEW SETTINGS (GAME ON)
+                                     //HIDE KEYBOARD OUTPUT WHEN GAME IS ON
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr); //GAME ON
+
     while(frame <101){
+
+        system("clear");
+        printf("FRAME: %d\n", frame);
         //FRAME START
+
         //USER INPUT FOR DIRECTION
-        direction = getchar();
+        direction = keyhit();
         switch(direction){
             case 'D': //RIGHT
                 if (headX == 9){
@@ -58,10 +72,23 @@ int main(){
             }
             printf("\n");
         }
-
-        printf("FRAME: %d\n", frame);
         frame++;
+        sleep(1);
     }
-
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr); //GAME OFF
     return 0;
 }
+/*
+int keyhit(){
+    struct termios oldattr, newattr; //define strucutre
+    int ch = '0'; //define character
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+*/
+
